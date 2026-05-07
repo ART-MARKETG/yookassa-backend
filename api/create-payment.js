bb354c2464d83fe2880086458b4ca8f91da9abce
ART-MARKETG <sergienkogordey@gmail.com>
2026-05-07 11:47:02 +0300

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -1,27 +1,25 @@
 export default async function handler(req, res) {
 
-  // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // OPTIONS
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
-  // Только POST
   if (req.method !== "POST") {
+
     return res.status(405).json({
       error: "Method not allowed"
     });
+
   }
 
   try {
 
     const { email, amount, plan } = req.body;
 
-    // Проверка
     if (!email || !amount) {
 
       return res.status(400).json({
@@ -30,19 +28,17 @@ export default async function handler(req, res) {
 
     }
 
-    // ЮKassa credentials
     const SHOP_ID = process.env.YOOKASSA_SHOP_ID;
     const SECRET_KEY = process.env.YOOKASSA_SECRET_KEY;
 
-    // AUTH
     const auth = Buffer
       .from(`${SHOP_ID}:${SECRET_KEY}`)
       .toString("base64");
 
-    // Создание платежа
     const response = await fetch(
       "https://api.yookassa.ru/v3/payments",
       {
+
         method: "POST",
 
         headers: {
@@ -65,7 +61,38 @@ export default async function handler(req, res) {
             return_url: "https://art-g.art"
           },
 
-          description: `Подписка ${plan} | ${email}`,
+          description: `Подписка ${plan}`,
+
+          receipt: {
+
+            customer: {
+              email: email
+            },
+
+            items: [
+
+              {
+
+                description: `Подписка ${plan}`,
+
+                quantity: "1.00",
+
+                amount: {
+                  value: Number(amount).toFixed(2),
+                  currency: "RUB"
+                },
+
+                vat_code: 1,
+
+                payment_mode: "full_payment",
+
+                payment_subject: "service"
+
+              }
+
+            ]
+
+          },
 
           metadata: {
             email,
@@ -88,7 +115,6 @@ export default async function handler(req, res) {
 
   } catch (e) {
 
-    console.log("SERVER ERROR:");
     console.log(e);
 
     return res.status(500).json({
